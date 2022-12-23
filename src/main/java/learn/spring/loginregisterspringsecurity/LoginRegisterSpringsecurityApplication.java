f0b59fe53007aa8bbbd0ee66ed53acc81e0d102f
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-23 23:11:48 +0545

    project initialized

---
+++ src/main/java/learn/spring/loginregisterspringsecurity/LoginRegisterSpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package learn.spring.loginregisterspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterSpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegisterSpringsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/spring/loginregisterspringsecurity/LoginRegisterSpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.loginregisterspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterSpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
