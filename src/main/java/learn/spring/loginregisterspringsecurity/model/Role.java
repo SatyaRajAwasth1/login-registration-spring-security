7e9553d872d52de1da97a6104dff6cf1bec1e84d
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-24 21:11:36 +0545

    jpa entities added along attributes

---
+++ src/main/java/learn/spring/loginregisterspringsecurity/model/Role.java
@@ -0,0 +1,28 @@
+package learn.spring.loginregisterspringsecurity.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/learn/spring/loginregisterspringsecurity/model/User.java
@@ -0,0 +1,66 @@
+package learn.spring.loginregisterspringsecurity.model;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+@Entity
+@Table (name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Collection<Role> roles;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+}
