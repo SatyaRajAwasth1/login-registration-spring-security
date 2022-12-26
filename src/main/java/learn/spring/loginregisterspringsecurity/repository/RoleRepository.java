6b5ff9654080158755d66b73354bbe335751a232
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-26 21:29:04 +0545

    ..

---
+++ src/main/java/learn/spring/loginregisterspringsecurity/repository/RoleRepository.java
@@ -0,0 +1,4 @@
+package learn.spring.loginregisterspringsecurity.repository;
+
+public interface RoleRepository {
+}
---
+++ src/main/java/learn/spring/loginregisterspringsecurity/service/UserService.java
@@ -0,0 +1,6 @@
+package learn.spring.loginregisterspringsecurity.service;
+
+// interface to provide services for student
+public interface UserService {
+
+}
