67e5e1b7713969d66aecabcd5581583d6d860c49
SatyaRajAwasth1 <awasthisatyaraj2060@gmail.com>
2022-12-28 22:46:17 +0545

    add studnet service added

--- src/main/java/learn/spring/loginregisterspringsecurity/service/UserService.java
+++ src/main/java/learn/spring/loginregisterspringsecurity/service/UserService.java
@@ -1,6 +1,10 @@
 package learn.spring.loginregisterspringsecurity.service;
 
+import learn.spring.loginregisterspringsecurity.model.User;
+import learn.spring.loginregisterspringsecurity.web.dto.UserRegistrationDto;
+
 // interface to provide services for student
 public interface UserService {
+    User add(UserRegistrationDto userRegistrationDto);
 
 }
---
+++ src/main/java/learn/spring/loginregisterspringsecurity/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package learn.spring.loginregisterspringsecurity.service;
+
+import learn.spring.loginregisterspringsecurity.model.Role;
+import learn.spring.loginregisterspringsecurity.model.User;
+import learn.spring.loginregisterspringsecurity.repository.UserRepository;
+import learn.spring.loginregisterspringsecurity.web.dto.UserRegistrationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+//implementation for the UserService
+@Service
+public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User add(UserRegistrationDto userRegistrationDto) {
+        User user = new User(userRegistrationDto.getFirstName(),userRegistrationDto.getLastName(),userRegistrationDto.getEmail(), userRegistrationDto.getPassword(), Arrays.asList(new Role("USER_ROLE")));
+        return null;
+    }
+}
