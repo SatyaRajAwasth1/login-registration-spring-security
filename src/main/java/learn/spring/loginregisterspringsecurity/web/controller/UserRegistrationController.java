1688ac89f379ab4714687e4871189a5ac2e01236
SatyaRajAwasth1 <awasthisatyaraj2060@gmail.com>
2022-12-29 23:54:03 +0545

    initiated

---
+++ src/main/java/learn/spring/loginregisterspringsecurity/web/controller/UserRegistrationController.java
@@ -0,0 +1,12 @@
+package learn.spring.loginregisterspringsecurity.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+// a controller class for user registration
+@Controller
+@RequestMapping("/registration")
+public class UserRegistrationController {
+
+}
