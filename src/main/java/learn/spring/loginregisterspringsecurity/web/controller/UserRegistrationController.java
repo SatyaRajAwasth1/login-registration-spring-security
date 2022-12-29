22fc0bffe86dce72216ebd5ee2aca53d0c0e4d46
SatyaRajAwasth1 <awasthisatyaraj2060@gmail.com>
2022-12-29 23:56:26 +0545

    controller defined

--- src/main/java/learn/spring/loginregisterspringsecurity/web/controller/UserRegistrationController.java
+++ src/main/java/learn/spring/loginregisterspringsecurity/web/controller/UserRegistrationController.java
@@ -1,5 +1,8 @@
 package learn.spring.loginregisterspringsecurity.web.controller;
 
+import learn.spring.loginregisterspringsecurity.model.User;
+import learn.spring.loginregisterspringsecurity.service.UserService;
+import learn.spring.loginregisterspringsecurity.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,5 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/registration")
 public class UserRegistrationController {
+    private UserService userService;
 
+    public UserRegistrationController(UserService userService) {
+        this.userService = userService;
+    }
 }
