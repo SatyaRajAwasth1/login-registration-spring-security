2205d30d0c44d1d18c4086448abc15e330a45007
SatyaRajAwasth1 <awasthisatyaraj2060@gmail.com>
2022-12-31 23:35:21 +0545

    ...

--- src/main/java/learn/spring/loginregisterspringsecurity/web/controller/UserRegistrationController.java
+++ src/main/java/learn/spring/loginregisterspringsecurity/web/controller/UserRegistrationController.java
@@ -4,16 +4,25 @@ import learn.spring.loginregisterspringsecurity.model.User;
 import learn.spring.loginregisterspringsecurity.service.UserService;
 import learn.spring.loginregisterspringsecurity.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 // a controller class for user registration
 @Controller
-@RequestMapping("/registration")
+@RequestMapping("/register")
 public class UserRegistrationController {
     private UserService userService;
 
     public UserRegistrationController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+
+    public String registerUser(UserRegistrationDto registrationDto){
+        userService.add(registrationDto);
+        return "redirect:/register?sucess"; // redirect to the page after registering
     }
 }
