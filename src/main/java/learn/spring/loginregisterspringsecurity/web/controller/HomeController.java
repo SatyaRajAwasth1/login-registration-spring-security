ac4882a77cffc830e365393b768e484637192fe1
SatyaRajAwasth1 <awasthisatyaraj2060@gmail.com>
2023-01-03 21:05:09 +0545

    home controller for root mapping

---
+++ src/main/java/learn/spring/loginregisterspringsecurity/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package learn.spring.loginregisterspringsecurity.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    String getHome(){
+        return "login";
+    }
+}
