fcdfb78878936907a5b56c8e2dd2131e72a6f7c5
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-01-11 22:33:49 +0545

    HttpSecurity configured

--- src/main/java/learn/spring/loginregisterspringsecurity/config/UserSecurityConfig.java
+++ src/main/java/learn/spring/loginregisterspringsecurity/config/UserSecurityConfig.java
@@ -1,11 +1,16 @@
 package learn.spring.loginregisterspringsecurity.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable
-@EnableWebSecurity
-public class UserSecurityConfig extends WebSecurityConfiguration {
-
+@Configuration
+public class UserSecurityConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((authz)-> authz.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
